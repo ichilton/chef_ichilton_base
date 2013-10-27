184ec6ce5e7892cb447b855e6ddd965c2118bcb9
Ian Chilton <ian@ichilton.co.uk>
2013-10-27 17:38:13 +0000

    Initial content in README.md and metadata.rb

--- README.md
+++ README.md
@@ -1,13 +1,20 @@
 # ichilton_base cookbook
 
+A Chef cookbook to do the base system setup common to all machines.
+
+
 # Requirements
+
 
 # Usage
 
+
 # Attributes
+
 
 # Recipes
 
+
 # Author
 
-Author:: YOUR_NAME (<YOUR_EMAIL>)
+Author:: Ian Chilton (<ian@ichilton.co.uk>)
--- metadata.rb
+++ metadata.rb
@@ -1,8 +1,9 @@
 name             'ichilton_base'
-maintainer       'YOUR_NAME'
-maintainer_email 'YOUR_EMAIL'
+maintainer       'Ian Chilton'
+maintainer_email 'ian@ichilton.co.uk'
 license          'All rights reserved'
-description      'Installs/Configures ichilton_base'
+description      'Chef cookbook containing base setup common to all nodes.'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
+supports 'ubuntu'
