8fcf2e039dddd4a13388d8610018907717e80eb7
Ian Chilton <ian@ichilton.co.uk>
2014-03-30 08:46:31 +0100

    Rename core recipe to system

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,7 +3,7 @@
 # Recipe:: default
 #
 
-include_recipe 'ichilton_base::core'
+include_recipe 'ichilton_base::system'
 include_recipe 'ichilton_base::packages'
 
 if node[:ichilton_base][:smtp_daemon][:enabled]
