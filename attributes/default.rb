b37cd466a5998719875e256d71895d6f4eb28800
Ian Chilton <ian@ichilton.co.uk>
2014-03-30 08:28:58 +0100

    Default to no SMTP daemon

--- attributes/default.rb
+++ attributes/default.rb
@@ -5,7 +5,7 @@
 
 default[:ichilton_base][:packages] = %w[ bzip2 cron curl dnsutils ftp git rsync screen telnet tmux traceroute vim wget zsh ]
 
-default[:ichilton_base][:smtp_daemon][:enabled] = true
+default[:ichilton_base][:smtp_daemon][:enabled] = false
 
 normal[:tz] = 'Europe/London'
 normal[:locale] = 'en_GB.utf8'
