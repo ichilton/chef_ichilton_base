d5b848cb1ac0be657fc67ff2be24fadf0a021521
Ian Chilton <ian@ichilton.co.uk>
2014-03-30 08:28:13 +0100

    Add traceroute package

--- attributes/default.rb
+++ attributes/default.rb
@@ -3,7 +3,7 @@
 # Attributes: default
 #
 
-default[:ichilton_base][:packages] = %w[ bzip2 cron curl dnsutils ftp git rsync screen telnet tmux vim wget zsh ]
+default[:ichilton_base][:packages] = %w[ bzip2 cron curl dnsutils ftp git rsync screen telnet tmux traceroute vim wget zsh ]
 
 default[:ichilton_base][:smtp_daemon][:enabled] = true
 
