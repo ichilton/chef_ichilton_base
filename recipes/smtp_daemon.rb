e359ecdc6ee97af9ea3e539a8dcdb9b88cdadd7c
Ian Chilton <ian@ichilton.co.uk>
2014-06-01 11:42:13 +0100

    Purge any exim packages from the image

--- recipes/smtp_daemon.rb
+++ recipes/smtp_daemon.rb
@@ -3,6 +3,11 @@
 # Recipe:: smtp_daemon
 #
 
+['exim4-base', 'exim4-config', 'exim4-daemon-light'].each do |pkg|
+  package pkg do
+    action :purge
+  end
+end
+
 # Use postfix cookbook to install:
 include_recipe 'postfix'
-
